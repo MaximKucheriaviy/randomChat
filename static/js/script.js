08c59e9758fe1e383e8f6bf228c68b3a20a89d88
Maxim Kucheriaviy <maxim-k@i.ua>
2022-06-28 11:04:33 +0300

    chat works

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,14 @@
 let socet;
-let name;
-const chatField = document.querySelector('.message-container');
-
+let userName;
+const windows = {
+    loginWindow: document.querySelector('.start-section'),
+    chatRoom: document.querySelector('.chat-room')
+}
 
 function createConnectLog(){
-    name = prompt("Add name");
     const message = {
         type: "Connect-log",
-        name
+        name: userName
     }
     return message;
 }
@@ -15,42 +16,63 @@ function createConnectLog(){
 function createMessage(text){
     const message = {
         type: "Message",
-        name,
+        name: userName,
         text
     }
     return message;
 }
 
-document.querySelector('.connect-button').onclick = (event) => {
+document.querySelector('.login-form__button').onclick = (event) => {
+    event.preventDefault();
+    userName = new FormData(document.forms.login).get('username');
+    if(userName === ""){
+        alert("Введите имя пользователя");
+        return;
+    }
     console.log("Socet open");
-    socet = new WebSocket('ws:/192.168.3.36:9000');
-
+    socet = new WebSocket('ws:/192.168.1.124:9000');
+    document.querySelector('.login-form').reset();
     socet.onopen = (event) => {
         socet.send(JSON.stringify(createConnectLog()));
+        windows.loginWindow.classList.toggle('visually-hidden');
+        windows.chatRoom.classList.toggle('visually-hidden');
     }
 
     socet.onmessage = (event) => {
-        const field = document.createElement('p');
-        field.textContent = event.data;
-        chatField.append(field);
+        console.log(event);
+        const arr = event.data.split(": ")
+        createMessageBlock(arr[0], arr[1]);
     }
 }
 
-document.querySelector('.send-button').onclick = (event) => {
+document.querySelector('.send-message-button').onclick = (event) => {
     event.preventDefault();
     if (socet) {
-        const data = new FormData(document.forms.textInputForm);
+        const data = new FormData(document.forms.messageForm);
         console.log(data.get("message"));
-        const field = document.createElement('p');
-        field.textContent = `${name}: ${data.get("message")}`;
-        chatField.append(field);
+        createMessageBlock(userName, data.get("message"), true);
         socet.send(JSON.stringify(createMessage(data.get("message"))));
     }
 }
 
-document.querySelector('.disconnect-button').onclick = (event) => {
-    if (socet) {
-        console.log("Socet cosed");
-        socet.close();
+// document.querySelector('.disconnect-button').onclick = (event) => {
+//     if (socet) {
+//         console.log("Socet cosed");
+//         socet.close();
+//     }
+// }
+
+function createMessageBlock(author, text, oun = false){
+    const block = document.createElement('div');
+    if(oun){
+        block.classList.add("message-block-oun");
     }
+    else{
+        block.classList.add("message-block-other");
+    }
+    const message = document.createElement('div');
+    message.classList.add('message-text-block')
+    message.textContent = `${author}: ${text}`;
+    block.append(message);
+    document.querySelector('.chatblock').append(block);
 }
