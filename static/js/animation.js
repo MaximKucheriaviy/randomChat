4f8a5af9ccb9300a99e02b24c06227d26c163de2
Maxim Kuchariaviy <maxim-k@i.ua>
2022-07-07 22:36:58 +0300

    added load scriean

---
+++ static/js/animation.js
@@ -0,0 +1,24 @@
+const loadingAnimation = {
+    div: document.querySelector('.loading-image'),
+    processId: undefined,
+    rotation: 0,
+    runAnimation() {
+        this.processId = setInterval(this.animation.bind(this), 70);
+        this.rotation = 0;
+    },
+    stopAnimation() {
+        clearInterval(this.processId);
+    },
+    
+    animation() {
+        this.rotation = this.rotation + 45;
+        if (this.rotation === 360) {
+            this.rotation = 0;
+        }
+        this.div.style.transform = `rotate(${this.rotation}deg)`;
+    }
+}
+
+
+document.querySelector('.start').addEventListener('click', event => {loadingAnimation.runAnimation()});
+document.querySelector('.stop').addEventListener('click', event => {loadingAnimation.stopAnimation()});
